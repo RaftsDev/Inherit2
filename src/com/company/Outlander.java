9d18ee152e1d405f889707460d23d45eee913b13
Ralph F <RaftsDev@gmail.com>
2020-09-02 03:03:40 -0400

    fix:constructor of Outlander class.

--- src/com/company/Outlander.java
+++ src/com/company/Outlander.java
@@ -3,7 +3,7 @@ package com.company;
 public class Outlander extends Car{
     private String serviceTime;
 
-    public Outlander(String name, String size, int wheels, int doors, int gears, boolean isManual, int currentGear, String serviceTime) {
+    public Outlander(String name, String size, String serviceTime) {
         super(name, size, 4, 5, 5, false, 0);
         this.serviceTime = serviceTime;
     }
