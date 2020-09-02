6f8ad0c6782db5405465fe23a91e0ffbfe448dbb
Ralph F <RaftsDev@gmail.com>
2020-09-01 20:47:20 -0400

    M:added methods to Vehicle class

--- src/com/company/Vehicle.java
+++ src/com/company/Vehicle.java
@@ -1,4 +1,28 @@
 package com.company;
 
 public class Vehicle {
+    private String name;
+    private String size;
+
+    private int currentVelocity;
+    private int currentDirection;
+
+    public Vehicle(String name, String size) {
+        this.name = name;
+        this.size = size;
+
+        this.currentVelocity=0;
+        this.currentDirection=0;
+    }
+
+    public void steer(int direction){
+        currentDirection += direction;
+        System.out.println("direction:"+currentDirection);
+    }
+
+    public void move(int velocity, int direction){
+        currentVelocity = velocity;
+        currentDirection = direction;
+        System.out.println("velocity:"+currentVelocity+" direction:"+currentDirection);
+    }
 }
