b67b757faae6b18b9d229035d509a3e478d38d9a
Ralph F <RaftsDev@gmail.com>
2020-09-03 11:30:21 -0400

    fix: Fixed typo.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Outlander outlander = new Outlander();
+        Outlander outlander = new Outlander(24);
         System.out.println("name:"+outlander.getName()+" size:"+outlander.getSize());
     }
 }
